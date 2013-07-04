9f5edf084e386616e404e3a6a5bd2d946e295487
James <james@jarofgreen.co.uk>
2013-07-04 15:15:54 +0100

    next

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
+
+
 $(document).ready(function() {
+	$( '#numberPpl' ).slider({ 
+		min: 1, 
+		max: 10, 
+		step: 1, 
+		value: 1,
+		change: function() { calculate(); }
+	});
+	$( '#numberLoos' ).slider({ 
+		min: 1, 
+		max: 10, 
+		step: 1, 
+		value: 1,
+		change: function() { calculate(); }
+	});
+
+
+
+	numberLoosSliders = $( '#numberLoos' ).slider({ min: 1, max: 10, step: 1, value: 1 });
+	
 	calculate();
 	$('#numberPpl').change(function() {
 		calculate();
@@ -10,11 +31,14 @@ $(document).ready(function() {
 
 
 function calculate() {
-	var numPpl = $('#numberPpl').val();
-	var numLoos = $('#numberLoos').val();
+	var numPpl = $( '#numberPpl' ).slider( "option", "value" );
+	var numLoos = $( '#numberLoos' ).slider( "option", "value" );
 	
 	var days = Math.round(16 / (numPpl  / numLoos));
 	
+	
+	$('#numberPplShow').html(numPpl);
+	$('#numberLoosShow').html(numLoos);
 	$('#changeDays').html(days);
 	
 };
